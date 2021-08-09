522366556c3b47dec81c83dcd05e25c3fa55e3c2
Xanthe McElroy <78177169+xanthemce@users.noreply.github.com>
2021-08-09 21:42:32 +0000

    program asks players higher or lower. prints whether the new number is higher or lower.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 
 card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-randomCard = Math.floor(Math.random() * card.length + 0)
+number = Math.floor(Math.random() * card.length + 0)
+randomCard = card[number]
+
+
+number1 = Math.floor(Math.random() * card.length + 0)
+randomCard1 = card[number1]
+number2 = Math.floor(Math.random() * card.length + 0)
+randomCard2 = card[number2]
+
+var player1Guess = prompt('player 1 guess higher or lower:')
+
+var player2Guess = prompt('player 2 guess higher or lower:')
+
+if(randomCard2>randomCard1){
+  console.log('it is higher')
+}
+else if(randomCard2<randomCard1){
+  console.log('it is lower')
+}
+
+else{
+  console.log('it is the same!')
+}
+
