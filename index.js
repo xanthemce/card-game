e51f1065f73725cc839c823cf38ff62770cec37d
Xanthe McElroy <78177169+xanthemce@users.noreply.github.com>
2021-08-09 21:56:00 +0000

    players can pick how many rounds they play for. prints if the player was wrong

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ card = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 point1 = 0
 point2 = 0
 round = 0
+ var playRound = prompt('how many rounds would you like to play for?')
+while ((isNaN(playRound)) || (playRound < 1) || (playRound == null) || (playRound == ' ') || (playRound % 1 != 0)) {
+  console.log('that is not a valid number. please enter a number')
+  playRound = prompt('how many rounds would you like to play for?')
+}
+
+
 
 
 do{
@@ -25,9 +32,18 @@ if(randomCard2>randomCard1){
     console.log('player 1 was right')
     point1++
     }
+  else if(player1Guess=='lower'){
+    console.log('player 1 was wrong')
+  }
+
+
+
   if (player2Guess=='higher'){
 console.log('player 2 was right')
 point2++
+  }
+  else if(player2Guess=='lower'){
+    console.log('player 2 was wrong')
   }
   
 }
@@ -37,9 +53,15 @@ else if(randomCard2<randomCard1){
     console.log('player 1 was right')
     point1++
     }
+  else if(player1Guess=='higher'){
+    console.log('player 1 was wrong')
+  }
   if (player2Guess=='lower'){
 console.log('player 2 was right')
 point2++
+  }
+  else if(player2Guess=='higher'){
+    console.log('player 2 was wrong')
   }
 }
 
@@ -48,7 +70,7 @@ else{
 
 }
 round++
-}while(round<2)
+}while(round<playRound)
 
 console.log('player 1 got ' + point1 + ' points')
 console.log('player 2 got ' + point2 + ' points')
