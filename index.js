2e51f224f055711169c76fe5983e20ba35b64dbe
Xanthe McElroy <78177169+xanthemce@users.noreply.github.com>
2021-08-11 02:09:33 +0000

    made it so the players can only choose to swap only 3 times. asks for name and picks one if they don't enter a name.

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
 
+let playerName1 = prompt('enter your name player 1:')
+let randomName = ['john', 'kate', 'hoob', 'randy','diig','jik','apples','orange','daisy','sunny','dandelion']
+
+let randomNumber1 = Math.floor(Math.random() * randomName.length + 0)
+let randomNumber2 = Math.floor(Math.random() * randomName.length + 0)
+
+let listRandomName1 = randomName[randomNumber1]
+
+if (playerName1 == ''){
+  console.log("Okay I will pick a name for you. Welcome to the game " + listRandomName1)
+  randomName.splice(randomNumber1, 1)
+}
+else (console.log("Welcome to the game " + playerName1))
+
+
+
+let listRandomName2 = randomName[randomNumber2]
+
+let playerName2 = prompt('enter your name player 2:')
+
+if (playerName2 == ''){
+  console.log("Okay I will pick a name for you. Welcome to the game " + listRandomName2)
+  randomName.splice(randomNumber2, 1)
+}
+else (console.log("Welcome to the game " + playerName2))
+
+let name1 = (playerName1 || listRandomName1)
+let name2 = (playerName2 || listRandomName2)
+
+
+
+
 
 card = ['an ace', 'a 2' , 'a 3' , 'a 4', 'a 5', 'a 6', 'a 7', 'an 8', 'a 9', 'a 10', 'a jack', 'a queen', 'a king']
 point1 = 0
 point2 = 0
 round = 0
 card1=0
+newCard = 0
 
  var playRound = prompt('how many rounds would you like to play for?')
 while ((isNaN(playRound)) || (playRound < 1) || (playRound == null) || (playRound == ' ') || (playRound % 1 != 0)) {
@@ -32,7 +65,9 @@ randomCard2 = card[number2]
 
 console.log('the first card is ' + randomCard1)
 
-var swap = prompt('would you like to draw another card? yes or no:')
+
+while(newCard<3){
+var swap = prompt('would you like to draw another card? you only get to do this three times. yes or no:')
 
 
 
@@ -44,24 +79,28 @@ if(swap=='yes'){
 number1 = Math.floor(Math.random() * card.length + 0)
 randomCard1 = card[number1]
 console.log('your new card is ' + randomCard1)
+newCard++
 }
-
-var player1Guess = prompt('player 1 guess higher or lower:')
+if(swap=='no'){
+  break;
+}
+}
+var player1Guess = prompt(name1 + ' guess higher or lower:')
 
 while (player1Guess != 'higher'&& player1Guess!='lower') {
   console.log('that is not a valid guess. please enter higher or lower')
-  player1Guess = prompt('player 1 guess higher or lower:')
+  player1Guess = prompt(name1 + ' guess higher or lower:')
 }
 
 
 
 
-var player2Guess = prompt('player 2 guess higher or lower:')
+var player2Guess = prompt(name2 + ' guess higher or lower:')
 
 
 while (player2Guess !='higher' && player2Guess!= 'lower') {
   console.log('that is not a valid guess. please enter higher or lower')
-  player2Guess = prompt('player 2 guess higher or lower:')
+  player2Guess = prompt(name2 + ' guess higher or lower:')
 }
 
 console.log('the first card was ' + randomCard1 + ' and the second card was ' + randomCard2)
@@ -70,39 +109,39 @@ console.log('the first card was ' + randomCard1 + ' and the second card was ' + 
 if(number2>number1){
   console.log('it is higher')
   if (player1Guess=='higher'){
-    console.log('player 1 was right')
+    console.log(name1 + ' was right')
     point1++
     }
   else if(player1Guess=='lower'){
-    console.log('player 1 was wrong')
+    console.log(name1 + ' was wrong')
   }
 
 
 
   if (player2Guess=='higher'){
-console.log('player 2 was right')
+console.log(name2 + ' was right')
 point2++
   }
   else if(player2Guess=='lower'){
-    console.log('player 2 was wrong')
+    console.log(name2 + ' was wrong')
   }
   
 }
 else if(number2<number1){
   console.log('it is lower')
   if (player1Guess=='lower'){
-    console.log('player 1 was right')
+    console.log(name1 + ' was right')
     point1++
     }
   else if(player1Guess=='higher'){
-    console.log('player 1 was wrong')
+    console.log(name1 + ' was wrong')
   }
   if (player2Guess=='lower'){
-console.log('player 2 was right')
+console.log(name2 + ' was right')
 point2++
   }
   else if(player2Guess=='higher'){
-    console.log('player 2 was wrong')
+    console.log(name2 + ' was wrong')
   }
 }
 
@@ -115,15 +154,15 @@ round++
 
 
 
-console.log('player 1 got ' + point1 + ' points')
-console.log('player 2 got ' + point2 + ' points')
+console.log(name1 + ' got ' + point1 + ' points')
+console.log(name2 + ' got ' + point2 + ' points')
 
 if(point1>point2){
-  console.log('player 1 has the most points and is the winner')
+  console.log(name1 + ' has the most points and is the winner')
 }
 
 else if(point1<point2){
-  console.log('player 2 has the most points and is the winner')
+  console.log(name2 + ' has the most points and is the winner')
 }
 
 else{
